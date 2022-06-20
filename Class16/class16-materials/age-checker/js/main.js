80d4f743da395281f643df2df5c26c59c8bba45a
eryan411 <eryan411@gmail.com>
2022-06-20 19:58:51 -0400

    completed homework for class 16

--- Class16/class16-materials/age-checker/js/main.js
+++ Class16/class16-materials/age-checker/js/main.js
@@ -6,8 +6,49 @@
 //If under 30, tell them they can not rent fancy cars affordably
 //If under over 30, tell them there is nothing left to look forward too
 
+document.querySelector('h1').addEventListener('click', checkAge)
+
+// function checkAge(){
+//     let age = document.querySelector('#danceDanceRevolution').value
+
+//     if(age < 16){
+//         alert(`ya can't drive lmao`)
+//     } else if (age < 18){
+//         alert(`ya can't hate from outside the club cause you can't even get in`)
+//     } else if (age < 21){
+//         alert(`ya can't drink lmao`)
+//     } else if (age < 25){
+//         alert(`ya can't rent cars affordably lmao`)
+//     } else if (age < 30){
+//         alert(`ya can't rent lambos affordably lmao`)
+//     } else if (age > 30){
+//         alert(`ya got nothing left to look forward too lmao`)
+//     } else {
+//         alert(`ya didn't enter a number lmao`)
+//     }
+// }
 
 //--- Harder
 //On click of the h1
 //Take the value from the input
 //Place the result of the conditional in the paragraph
+
+function checkAge(){
+    let age = document.querySelector('#danceDanceRevolution').value
+
+    if(age < 16){
+        document.querySelector('p').innerHTML = (`ya can't drive lmao`)
+    } else if (age < 18){
+        document.querySelector('p').innerHTML = (`ya can't hate from outside the club cause you can't even get in`)
+    } else if (age < 21){
+        document.querySelector('p').innerHTML = (`ya can't drink lmao`)
+    } else if (age < 25){
+        document.querySelector('p').innerHTML = (`ya can't rent cars affordably lmao`)
+    } else if (age < 30){
+        document.querySelector('p').innerHTML = (`ya can't rent lambos affordably lmao`)
+    } else if (age > 30){
+        document.querySelector('p').innerHTML = (`ya got nothing left to look forward too lmao`)
+    } else {
+        document.querySelector('p').innerHTML = (`ya didn't enter a number lmao`)
+    }
+}
