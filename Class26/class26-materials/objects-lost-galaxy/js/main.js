184ac7431147202620674e7e3d21991c37912d01
eryan411 <eryan411@gmail.com>
2022-06-22 22:11:39 -0400

    daily upload

--- Class26/class26-materials/objects-lost-galaxy/js/main.js
+++ Class26/class26-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,21 @@
 //Create a dog object that has four properties and three methods
+
+
+let pizza = {}
+
+pizza.size = 'large'
+pizza.toppings = ['spinch', 'onion', 'jalapenos', 'banana peppers', ' garlic']
+pizza.crust = 'stuffed'
+pizza.sauce = 'heavy'
+
+pizza.estimatedDeliveryTime = function(){
+    console.log('Calculating...')
+}
+
+pizza.burnMouth = function(){
+    console.log('SDALHFJKDLSAHJSDF')
+}
+
+pizza.frisbee = function(){
+    console.log('YEEEeeettt')
+}
