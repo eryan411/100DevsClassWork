7e251a6800cd9caa3f5f3072073e0b2f99b21eeb
eryan411 <eryan411@gmail.com>
2022-06-22 23:30:54 -0400

    changes

--- Class38/rap-names-api/server.js
+++ Class38/rap-names-api/server.js
@@ -2,18 +2,39 @@ const express = require('express')
 const app = express()
 const PORT = 8000
 
-const savage = {
-    'age': 29,
-    'birthname': 'Sheyaa Bin Abraham-Joseph',
-    'birthlocation': 'London, England',
+const rappers = {
+    '21 Savage':
+        {'age': 29,
+        'birthname': 'Sheyaa Bin Abraham-Joseph',
+        'birthlocation': 'London, England'
+    },
+    'Chance The Rapper':{
+        'age': 29,
+        'birthname': 'Chancelor Bennett',
+        'birthlocation': 'Chicago, Illinois',
+    },
+    'Dylan':{
+        'age': 29,
+        'birthname': 'Dylan',
+        'birthlocation': 'Dylan',
+    }
 }
 
 app.get('/', (req, res)=>{
     res.sendFile(__dirname + '/index.html')
 })
 
-app.get('/api', (req,res)=>{
-    res.json()
+// app.get('/api', (req,res)=>{
+//     res.json(rappers)
+// })
+
+app.get('/api/:rapperName', (req, res) => {
+    const rappersName = req.params.rapperName
+    if(rappers[rappersName]){
+        res.json(rappers[rappersName])
+    }else{
+        res.json(rappers['Dylan'])
+    }
 })
 
 app.listen(PORT, ()=>{
