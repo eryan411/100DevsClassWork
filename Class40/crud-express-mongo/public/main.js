bb8f467f993606fa43f7b2987b3fb3b8ab584488
eryan411 <eryan411@gmail.com>
2022-06-25 14:34:06 -0400

    birthday work

--- Class40/crud-express-mongo/public/main.js
+++ Class40/crud-express-mongo/public/main.js
@@ -1,5 +1,10 @@
+const { application } = require("express")
+
 const update = document.querySelector('#update-button')
 
 update.addEventListener('click', _ => {
-    
+    fetch('/quotes', {
+        method: 'put',
+        headers: { 'Content-Type': 'application/json'},
+    })
 })
