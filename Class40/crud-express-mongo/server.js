29546084fb4669a3e9a6c01b21621d134592fabf
eryan411 <eryan411@gmail.com>
2022-06-26 16:57:34 -0400

    working through crud tutorial

--- Class40/crud-express-mongo/server.js
+++ Class40/crud-express-mongo/server.js
@@ -1,38 +1,75 @@
 const express = require('express');
 const bodyParser = require('body-parser')
-const app = express();
-const PORT = 3000
 const MongoClient = require('mongodb').MongoClient
+const app = express();
+
+const PORT = 3000
+
 const connectionString = 'mongodb+srv://eryan411:testserver123@cluster0.701bxt1.mongodb.net/?retryWrites=true&w=majority'
 
 
 
-MongoClient.connect(connectionString, {
-    useUnifiedTopology: true
-}).then(client => {
-    console.log('Connected to Database')
-    const db = client.db('star-wars-quotes')
-    const quotesCollection = db.collection('quotes')
-    app.set('view engine', 'ejs')
-    app.use(bodyParser.urlencoded({ extended: true }))
-    app.use(express.static('public'))
-    app.listen(PORT, function(){
-        console.log(`listening on ${PORT}`)
-    })
-    app.get('/', (req, res) => {
-        db.collection('quotes').find().toArray()
-        .then(results => {
-            res.render('index.ejs', { quotes: results })
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+    .then(client => {
+        console.log('Connected to Database')
+        const db = client.db('star-wars-quotes')
+        const quotesCollection = db.collection('quotes')
+    
+    
+        app.set('view engine', 'ejs')
+        app.use(bodyParser.urlencoded({ extended: true }))
+        app.use(express.static('public'))
+        app.use(bodyParser.json())
+        app.listen(PORT, function(){
+            console.log(`listening on ${PORT}`)
         })
-        .catch(error => console.error(error))
-    })
-    app.post('/quotes', (req, res) => {
-        quotesCollection.insertOne(req.body)
-        .then(result => {
-            res.redirect('/')
+
+
+        app.get('/', (req, res) => {
+            db.collection('quotes').find().toArray()
+            .then(quotes => {
+                res.render('index.ejs', { quotes: quotes })
+            })
+            .catch(error => console.error(error))
         })
-        .catch(error => console.error(error))
-    })
+
+        app.post('/quotes', (req, res) => {
+            quotesCollection.insertOne(req.body)
+            .then(result => {
+                res.redirect('/')
+            })
+            .catch(error => console.error(error))
+        })
+
+        app.put('/quotes', (req, res) => {
+            quotesCollection.findOneAndUpdate(
+                {name: 'Yoda'},
+                {
+                    $set: {
+                        name: req.body.name,
+                        quote: req.body.quote
+                    }
+                },
+                {
+                    upsert: true
+                }
+            )
+            .then(result => console.log(result))
+            .catch(error => console.error(error))
+        })
+
+        app.delete('/quotes', (req, res) => {
+            quotesCollection.deleteOne(
+                { name: req.body.name }
+            )
+            .then(result => {
+                if(result.deletedCount === 0) {
+                    return res.json('No quote to delete')
+                }
+                res.json(`Deleted Darth Vader's quote`)
+            })
+            .catch(error => console.error(error))
+        })
 })
 .catch(console.error)
 
